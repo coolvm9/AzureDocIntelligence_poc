eccb667a325dab8684ca5deae9d1ff7722490af2
coolvm9 <coolvm9@gmail.com>
2024-12-03 11:43:49 -0800

    Updates to project

--- src/main/java/dev/fusion/AZDocIntelligenceAsync.java
+++ src/main/java/dev/fusion/AZDocIntelligenceAsync.java
@@ -13,6 +13,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 public class AZDocIntelligenceAsync {
@@ -45,6 +47,17 @@ public class AZDocIntelligenceAsync {
                     .credential(new AzureKeyCredential(key))
                     .endpoint(endpoint)
                     .buildAsyncClient();*/
+
+            List<DocumentAnalysisFeature> features = Arrays.asList(
+                    DocumentAnalysisFeature.FORMULAS,
+                    DocumentAnalysisFeature.KEY_VALUE_PAIRS,
+                    DocumentAnalysisFeature.BARCODES
+            );
+            List<String> queryFields = Arrays.asList(
+                    "InvoiceDate",
+                    "InvoiceTotal",
+                    "VendorName"
+            );
 
             // Load the PDF file
             File file = new File(DocumentIntelligenceClient.class.getClassLoader().getResource("sample-layout.pdf").getFile());
@@ -83,27 +96,55 @@ public class AZDocIntelligenceAsync {
                 AnalyzeResult analyzeLayoutResult = analyzeLayoutResultPoller.getFinalResult();
                 // Print the result (you can further process it as needed)
                 System.out.println("Analyze Result: " + analyzeLayoutResult.toJsonString());
-                // Print Page Details
-                analyzeLayoutResult.getPages().forEach(page -> {
-                    System.out.println("Page Number: " + page.getPageNumber());
-                    page.getLines().forEach(line -> {
-                        System.out.println("Line: " + line.getContent());
+
+                analyzeLayoutResult.getParagraphs().forEach(documentParagraph ->
+                        System.out.printf("Paragraph '%s' is within a bounding box %s.%n and role is %s \n",
+                                documentParagraph.getContent(),
+                                documentParagraph.getBoundingRegions().toString(), documentParagraph.getRole()));
+
+                analyzeLayoutResult.getTables().forEach(documentTable -> {
+                    System.out.printf("Table has %d rows and %d columns.%n", documentTable.getRowCount(),
+                            documentTable.getColumnCount());
+                    documentTable.getCells().forEach(documentTableCell -> {
+                        System.out.printf("Cell '%s', has row index %d and column index %d.%n", documentTableCell.getContent(),
+                                documentTableCell.getRowIndex(), documentTableCell.getColumnIndex());
                     });
+                });
+                // pages
+
+                analyzeLayoutResult.getPages().forEach(documentPage -> {
+                    System.out.printf("Page has width: %.2f and height: %.2f, measured with unit: %s%n",
+                            documentPage.getWidth(),
+                            documentPage.getHeight(),
+                            documentPage.getUnit());
+
+                    // lines
+                    documentPage.getLines().forEach(documentLine ->
+                            System.out.printf("Line '%s' is within a bounding box %s.%n",
+                                    documentLine.getContent(),
+                                    documentLine.getPolygon().toString()));
+
+
+                    // selection marks
+                    documentPage.getSelectionMarks().forEach(documentSelectionMark ->
+                            System.out.printf("Selection mark is '%s' and is within a bounding box %s with confidence %.2f.%n",
+                                    documentSelectionMark.getState().toString(),
+                                    documentSelectionMark.getPolygon().toString(),
+                                    documentSelectionMark.getConfidence()));
                 });
 
-                // Print Paragraphs
-                analyzeLayoutResult.getParagraphs().forEach(paragraph -> {
-                    System.out.println("Paragraph: " + paragraph.getContent() + ", Role : " + paragraph.getRole());
-                });
-                // Print Tables
-                analyzeLayoutResult.getTables().forEach(table -> {
-                    System.out.println("Table with " + table.getRowCount() + " rows and " + table.getColumnCount() + " columns");
-                    table.getCells().forEach(cell -> {
-                        System.out.println("Cell Row: " + cell.getRowIndex() +
-                                ", Column: " + cell.getColumnIndex() +
-                                ", Content: " + cell.getContent());
+// tables
+                List<DocumentTable> tables = analyzeLayoutResult.getTables();
+                for (int i = 0; i < tables.size(); i++) {
+                    DocumentTable documentTable = tables.get(i);
+                    System.out.printf("Table %d has %d rows and %d columns.%n", i, documentTable.getRowCount(),
+                            documentTable.getColumnCount());
+                    documentTable.getCells().forEach(documentTableCell -> {
+                        System.out.printf("Cell '%s', has row index %d and column index %d.%n", documentTableCell.getContent(),
+                                documentTableCell.getRowIndex(), documentTableCell.getColumnIndex());
                     });
-                });
+                    System.out.println();
+                }
 
 
             } else {
